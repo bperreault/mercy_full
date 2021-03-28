373c30f6b7a5adefc461a803dd9028b482298b77
bperreault <bcp@faxt.com>
2021-03-28 16:07:05 -0400

    donate button

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -51,8 +51,13 @@ Item donations are accepted as well and can be picked up at your convenience.<br
 </div>
       </div>
     
-    <img src="/images/mcfPaypalQR.jpg" style="border:0" />
-      
+    <img style="max-height:100px" src="/images/mcfPaypalQR.jpg" style="border:0" />
+      <br />
+      <form action="https://www.paypal.com/donate" method="post" target="_top">
+<input type="hidden" name="hosted_button_id" value="3FFLHTBJRDAD6" />
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+</form>
      
 </div>
   </div>

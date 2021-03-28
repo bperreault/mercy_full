a8005801e1c3bb50dc81349a20df0226bcd2089d
bperreault <bcp@faxt.com>
2021-03-28 16:45:46 -0400

    .

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -48,18 +48,11 @@ Donations made
 Through PayPay to mercyfullfarms@gmail.com
 Item donations are accepted as well and can be picked up at your convenience.<br><br>
      
-    
-  
-<form action="https://www.paypal.com/donate" method="post" target="_top">
-<input type="hidden" name="hosted_button_id" value="ZNGK375XYMMPJ" />
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" style="border:0;" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-</form>
 
 
-
+<a href="https://www.paypal.com/donate?hosted_button_id=MWNK84XLYP832">
 <img style="border:0; margin:auto; text-align:center;" src="/images/QRCode.png"  />
-      
+</a>   
       <br />
 
 </div>
